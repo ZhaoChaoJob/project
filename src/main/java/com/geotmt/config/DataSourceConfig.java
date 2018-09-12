9746cf8c9396bf3b20e7d351120c435c2a45ac4c
zhaochao <albg_boy@163.com>
2018-09-12 18:41:32 +0800

    [ 多数据源配置 | 成功 ]

--- src/main/java/com/geotmt/config/DataSourceConfig.java
+++ src/main/java/com/geotmt/config/DataSourceConfig.java
@@ -2,7 +2,6 @@ package com.geotmt.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,7 +25,7 @@ public class DataSourceConfig {
      * @return 数据源
      */
     @Bean
-//    @Profile(value = {"dev"})
+    @Profile(value = {"dev"})
     public DataSource DevDataSource() {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
@@ -35,36 +34,36 @@ public class DataSourceConfig {
         return dataSource;
     }
 
-//    /**
-//     * 测试开发环境
-//     * 说明：生产环境因常年不动，故写死
-//     *
-//     * @return 数据源
-//     */
-//    @Bean
-//    @Profile(value = {"test"})
-//    public DataSource TestDataSource() {
-//        DruidDataSource dataSource = new DruidDataSource();
-//        dataSource.setUrl("写死测试环境");
-//        dataSource.setUsername("写死测试环境");
-//        dataSource.setPassword("写死测试环境");
-//        return dataSource;
-//    }
-//
-//    /**
-//     * 生产开发环境
-//     * 说明：生产环境因常年不动，故写死
-//     *
-//     * @return 数据源
-//     */
-//    @Bean
-//    @Profile(value = {"pro"})
-//    public DataSource ProDataSource() {
-//        DruidDataSource dataSource = new DruidDataSource();
-//        dataSource.setUrl("写死生产");
-//        dataSource.setUsername("写死生产");
-//        dataSource.setPassword("写死生产");
-//        return dataSource;
-//    }
+    /**
+     * 测试开发环境
+     * 说明：生产环境因常年不动，故写死
+     *
+     * @return 数据源
+     */
+    @Bean
+    @Profile(value = {"test"})
+    public DataSource TestDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("写死测试环境");
+        dataSource.setUsername("写死测试环境");
+        dataSource.setPassword("写死测试环境");
+        return dataSource;
+    }
+
+    /**
+     * 生产开发环境
+     * 说明：生产环境因常年不动，故写死
+     *
+     * @return 数据源
+     */
+    @Bean
+    @Profile(value = {"pro"})
+    public DataSource ProDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/apploan?useUnicode=true&characterEncoding=UTF-8&useOldAliasMetadataBehavior=true&useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
 
 }
