1023ccad5621288089004e7b2905fc2bf96ffa8a
zhaochao <albg_boy@163.com>
2018-09-17 16:40:01 +0800

    [ lombok | 少些一些乱七八糟的代码 ]

--- src/main/java/com/geotmt/admin/model/mongodb/Persion.java
+++ src/main/java/com/geotmt/admin/model/mongodb/Persion.java
@@ -1,28 +1,16 @@
 package com.geotmt.admin.model.mongodb;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  *
  * Created by geo on 2018/9/13. */
 @Document(collection="Persion")
+@Getter
+@Setter
 public class Persion {
     private String _id ;
     private String name ;
-
-    public String get_id() {
-        return _id;
-    }
-
-    public void set_id(String _id) {
-        this._id = _id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
