fa57db41eca4ff98b1bfd4b4e09e76c28224724d
zhaochao <albg_boy@163.com>
2018-09-18 18:44:19 +0800

    [ 代碼生成器 | 如果想独立出去，需要的jar  ]

--- src/main/java/com/geotmt/generator/GeneratorMain.java
+++ src/main/java/com/geotmt/generator/GeneratorMain.java
@@ -26,19 +26,9 @@ public class GeneratorMain {
 	//  g.printAllTableNames();				//打印数据库中的表名称
 		g.clean();							//删除生成器的输出目录
 //		g.generateByTable("T_C_TASK_INFO","TaskInfo");	//通过数据库表生成文件,并可以自定义类名,通过数据库表生成文件,注意: oracle 需要指定schema及注意表名的大小写.
-//		g.generateByTable("T_B_ORG","Org");
-//		g.generateByTable("t_r_warn_log".toUpperCase(),"WarnLog");
-//		g.generateByTable("T_C_DZSQL","DzSql");
-//		g.generateByTable("T_C_EXCUTE_LOG_INFO","ExcuteLogInfo");
-//		g.generateByTable("t_c_ftp_info".toUpperCase(),"FtpInfo");
-//		g.generateByTable("T_C_RES_COLUMN_INFO".toUpperCase(),"ResColumnInfo");
-//		g.generateByTable("t_c_res_file_info".toUpperCase(),"ResFileInfo");
-//		g.generateByTable("t_c_task_file".toUpperCase(),"TaskFile");
-//		g.generateByTable("t_l_eiss_acc_error_ls".toUpperCase(),"EissAccErrorls");
-//		g.generateByTable("t_l_ei_acc_balance_sum".toUpperCase(),"EiAccBalanceSum");
-//		g.generateByTable("t_l_ei_acc_error_ls".toUpperCase(),"EiAccErrorLs");
-//		g.generateByTable("T_B_SYS_CONFIG","SysConfig"); 
 		g.generateByTable("t_sys_user".toUpperCase(),"SysUser");
+		g.generateByTable("t_sys_role".toUpperCase(),"SysRole");
+		g.generateByTable("t_sys_permission".toUpperCase(),"SysPermission");
 //		g.generateByTable("t_l_eiss_acc_balance_sum".toUpperCase(),"EissAccBlanceSum"); 
 	//  g.generateByTable("V_bankeq_PUNISH_ALL_JSP","TbankeqPunishQuery","ID");//通过数据库视图生成文件,并可以自定义类名,最后参数为视图主键
 	//	g.generateByAllTable();				//自动搜索数据库中的所有表并生成文件
