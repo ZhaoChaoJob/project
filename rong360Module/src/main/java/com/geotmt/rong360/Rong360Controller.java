0a842f2b4f8cd5eb7310b1b12ec199d7a5cf7f2d
zhaochao <albg_boy@163.com>
2018-09-25 14:53:53 +0800

    [  更新 | 注释  ]

--- rong360Module/src/main/java/com/geotmt/rong360/Rong360Controller.java
+++ rong360Module/src/main/java/com/geotmt/rong360/Rong360Controller.java
@@ -6,19 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by geo on 2018/9/25.
- */
+ * 融360接口
+ *
+ * Created by chao.zhao on 2018/9/25. */
 @Controller
 public class Rong360Controller {
     /**
      * ajax登录请求接口方式登陆
-     * @param username
-     * @param password
-     * @return
      */
     @RequestMapping(value="/rong360")
     @ResponseBody
-    public String rong360(String username, String password, Model model) {
-        return "这是融360的接口";
+    public String rong360() {
+        return "this is rong360 api !!";
     }
 }
